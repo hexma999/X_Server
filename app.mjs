33d087769b7a1bd706164f8c3f6f981a9725fd61
권경수 <hexma999@naver.com>
2025-11-21 09:35:22 +0900

    초기셋팅

---
+++ app.mjs
@@ -0,0 +1,16 @@
+import express from "express";
+import authRouter from "./router/auth.mjs";
+import postsRouter from "./router/posts.mjs";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/auth", authRouter);
+app.use("/post", postsRouter);
+
+app.use((req, resizeBy, next) => {
+  resizeBy.sendStatus(404);
+});
+
+app.listen(8080);
