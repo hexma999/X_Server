04b5bb008ca62dab8f0f885e3db5b07cb819e23b
권경수 <hexma999@naver.com>
2025-11-24 09:41:43 +0900

    jwt 토큰 적용

--- middleware/auth.mjs
+++ middleware/auth.mjs
@@ -14,7 +14,21 @@ export const isAuth = async (req, res, next) => {
 
   //Authorization:Bearer WERWEFWVDSGEWREWREWRFWERF==
   const token = authHeader.split(" ")[1]; //WERWEFWVDSGEWREWREWRFWERF==
-  console.log("토큰 분리 성공", token);
-
-  next();
+  //console.log("토큰 분리 성공", token);
+  jwt.verify(token, "abcdefg1234!@#$", async (error, decoded) => {
+    if (error) {
+      console.log("토큰 에러");
+      return res.status(401).json(AUTH_ERROR);
+    }
+    console.log(decoded);
+    const user = await authRepository.findById(decoded.id);
+    if (!user) {
+      console.log("아이디 없음");
+      return res.status(401).json(AUTH_ERROR);
+    }
+    console.log("user.id: ", user.id);
+    console.log("use.userid: ", user.userid);
+    req.userid = user.userid;
+    next();
+  });
 };
